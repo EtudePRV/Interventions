ad3cdad9d93126dbed340afc0550a520206a7230
Pierre-Rafael Vellone <pierre-rafael@hotmail.com>
2019-03-13 13:21:12 -0400

    Tp6
    Signed-off-by: Pierre-Rafael Vellone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProblemeComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    AngularFontAwesomeModule,
     AppRoutingModule
   ],
   providers: [],
