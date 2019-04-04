e32e71ee5b80787d7ed4b7c7ea10ca9fd26b0fd1
Pierre-Rafael Vellone <killer_paul_10@hotmail.com>
2019-04-04 15:11:17 -0400

    tp16 fini Signed-off-by: Pierre-Rafael Vellone <killer_paul_10@hotmail.com>

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -27,6 +27,9 @@ export class ProblemeComponent implements OnInit {
           courrielConfirmation: [{value:'', disabled:true}],
           },{validator: emailMatcherValidator.courrielDifferents()}),
       telephone: [{value:'', disabled:true}],
+      descriptionProbleme: ['', [Validators.required, Validators.minLength(5)]],
+      noUnite: '',
+      dateProbleme: {value: Date(), disabled: true} 
      });
      
      this.types.obtenirTypes()
