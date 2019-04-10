8c94f2b562801a4037a79898be5c6cd1fc2c8a0d
Pierre-Rafael Vellone <killer_paul_10@hotmail.com>
2019-04-10 14:37:28 -0400

    p17 fini Signed-off-by: Pierre-Rafael Vellone

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,40 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('stocks App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('Doit afficher le titre du formulaire Déclarer un problème', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to Interventions!');
+    expect(page.getParagraphText()).toEqual('Déclarer un problème');
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+  it('Doit activer le bouton Sauvegarderavec champs valides scénario nominal', () => {
+    page.setChampsValidesScenarioNominal();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });  
+  
+  it('Doit activer le bouton Sauvegarder avec champs valides scénario alternatifPar message TEXTE', () => {
+    page.setChampsValidesScenarioAlternatifParMessageTexte();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });
+
+  it('Doit activer le bouton Sauvegarderavec champs valides scénario alternatif Par courriel', () => {
+    page.setChampsValidesScenarioAlternatifParCourriel();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });
+
+  it('Zone DESCRIPTION DU PROBLÈME a une bordure VERTE si nombre de caractères suffisant', () => {
+    page.setZoneDescriptionCaracteresSuffisant();  
+    expect(page.obtenirClasseZoneDescription()).toContain('is-valid');
+  });
+
+  it('Zone DESCRIPTION DU PROBLÈME a une bordure ROUGE si nombre de caractères insuffisant ', () => {
+    page.setZoneDescriptionCaracteresInsuffisant();  
+    expect(page.obtenirClasseZoneDescription()).toContain('is-invalid');
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,66 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+    return browser.get('/probleme');
   }
 
-  getTitleText() {
-    return element(by.css('Inter-root h1')).getText() as Promise<string>;
+  getParagraphText() {
+    return element(by.css('Inter-root h5')).getText();
   }
+
+  setChampsValidesScenarioNominal() : void {
+    element(by.id('prenomProblemeId')).sendKeys('Joe');
+    element(by.id('nomProblemeId')).sendKeys('Gage');    
+    // Sélectionner le premier élément dans la zone de liste déroulante
+    element(by.id('listTypeProblemeId')).all(by.tagName('option')).get(1).click();      
+    // Cliquer sur la première option du bouton radio
+    element.all(by.id('ContactId')).get(0).click();   
+    element(by.id('noUniteId')).sendKeys('1');
+    element(by.id('descriptionProblemeId')).sendKeys('Maiders');   
+  }
+
+  setChampsValidesScenarioAlternatifParMessageTexte(): void {
+    element(by.id('prenomProblemeId')).sendKeys('Joe');
+    element(by.id('nomProblemeId')).sendKeys('Gage');    
+    // Sélectionner le premier élément dans la zone de liste déroulante
+    element(by.id('listTypeProblemeId')).all(by.tagName('option')).get(1).click();      
+    // Cliquer sur la première option du bouton radio
+    element.all(by.id('ContactId')).get(2).click();   
+    element(by.id('noUniteId')).sendKeys('1');
+    element(by.id('telephoneId')).sendKeys('1514123123');
+    element(by.id('descriptionProblemeId')).sendKeys('Maiders');
+    
+  }
+
+  setChampsValidesScenarioAlternatifParCourriel(): void {
+    element(by.id('prenomProblemeId')).sendKeys('Joe');
+    element(by.id('nomProblemeId')).sendKeys('Gage');    
+    // Sélectionner le premier élément dans la zone de liste déroulante
+    element(by.id('listTypeProblemeId')).all(by.tagName('option')).get(1).click();      
+    // Cliquer sur la première option du bouton radio
+    element.all(by.id('ContactId')).get(1).click(); 
+    element(by.id('courrielId')).sendKeys('joe@gage.com');  
+    element(by.id('courrielConfirmationId')).sendKeys('joe@gage.com');  
+    element(by.id('noUniteId')).sendKeys('1');
+    element(by.id('descriptionProblemeId')).sendKeys('Maiders');
+    
+  }
+  boutonSubmit() : ElementFinder { 
+    return element(by.buttonText('Sauvegarder'));
+  }   
+
+  setZoneDescriptionCaracteresSuffisant() : void {
+    element(by.id('descriptionProblemeId')).clear();
+    element(by.id('descriptionProblemeId')).sendKeys('XXXXX');
+  }
+
+  setZoneDescriptionCaracteresInsuffisant() : void {
+    element(by.id('descriptionProblemeId')).clear();
+    element(by.id('descriptionProblemeId')).sendKeys('XXX');
+  }
+
+  obtenirClasseZoneDescription()   { 
+    return element(by.id('descriptionProblemeId')).getAttribute("class");
+  }   
 }
